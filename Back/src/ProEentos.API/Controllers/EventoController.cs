ecdb5ed906592783ef3945004ea0e5734581b413
Bruno de Souza Pinto Marques Co <Bruno.Souza.Correa@ibm.com>
2022-02-24 19:56:22 -0300

    primeiro commit

---
+++ Back/src/ProEentos.API/Controllers/EventoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEentos.API.Model;
+
+
+namespace ProEentos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {      
+
+        
+        public EventoController()
+        {
+            
+        }
+    
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+
+                    EventoId = 1,
+                    Tema = "Angular 11 ",
+                    Local ="Petropolis",
+                    DataEvento = "12-12-2022",
+                    QtdPessoas = 1
+
+
+            };
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "value";
+        }
+    }
+}
