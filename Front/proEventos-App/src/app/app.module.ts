423ce6729c3e6c8db3aa8d8e254af3f048508736
Bruno de Souza Pinto Marques Co <Bruno.Souza.Correa@ibm.com>
2022-03-04 19:37:22 -0300

    secao 5

--- Front/proEventos-App/src/app/app.module.ts
+++ Front/proEventos-App/src/app/app.module.ts
@@ -6,17 +6,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent
+    EventosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
 
   ],
   providers: [],
--- Front/proEventos-App/src/app/eventos/eventos.component.ts
+++ Front/proEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number =150;
+  marginImg: number =2;
+  showImg = false;
+  private _filtroLista: string = '';
+
+  public get filtroLista():string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
 
   constructor(private http:HttpClient) { }
 
@@ -16,13 +30,30 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  filtrarEventos(palavraFiltro: string):any{
+    palavraFiltro = palavraFiltro.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(palavraFiltro) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(palavraFiltro) !== -1
+    )
+  }
+
+  public showImage(){
+
+    this.showImg =  !this.showImg;
+  }
+
   public getEventos():void{
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
+        response=>{
+          this.eventos = response;
+          this.eventosFiltrados = this.eventos;
 
-        response=>this.eventos = response,
+        },
         error=> console.log(error)
     );
   }
+
 
 }
